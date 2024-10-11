3c6b2628094e70d3eb0091606ac742df8e2b026c
deiz0n <eduardomacedosilva@outlook.com>
2024-10-11 18:41:50 -0300

    feat: implementação completa da lista encadeada simples

--- listas-encadeadas/lista-encadeada.py
+++ listas-encadeadas/lista-encadeada.py
@@ -11,3 +11,68 @@ class No:
 class ListaEncadeada:
     def __init__(self):
         self.primeiro = None
+
+    def insere_inicio(self, value):
+        novo = No(value)
+        novo.proximo = self.primeiro
+        self.primeiro = novo
+
+    def mostrar(self):
+        self.__is_empty()
+        atual = self.primeiro
+        while atual is not None:
+            atual.mostra_no()
+            atual = atual.proximo
+
+    def excluir_inicio(self):
+        temp = self.primeiro
+        self.__is_empty()
+        self.primeiro = self.primeiro.proximo
+        return temp
+
+    def pesquisar(self, value):
+        self.__is_empty()
+        atual = self.primeiro
+        while atual.value != value:
+            if atual.proximo is None:
+                print('Value not found')
+                return None
+            atual = atual.proximo
+        return atual
+
+    def excluir_posicao(self, value):
+        self.__is_empty()
+        atual = self.primeiro
+        anterior = self.primeiro
+        while atual.value != value:
+            if atual.proximo is None:
+                return None
+            else:
+                anteior = atual
+                atual = atual.proximo
+
+        if atual == self.primeiro:
+            self.primeiro = self.primeiro.proximo
+        else:
+            anteior.proximo = atual.proximo
+
+    def __is_empty(self):
+        if self.primeiro.proximo is None:
+            print('List is empty')
+            return None
+
+if __name__ == "__main__":
+    lista = ListaEncadeada()
+    lista.insere_inicio(1)
+    lista.insere_inicio(2)
+    lista.insere_inicio(3)
+    lista.insere_inicio(4)
+    lista.insere_inicio(5)
+
+    lista.excluir_posicao(3)
+    lista.excluir_posicao(5)
+    lista.excluir_posicao(1)
+
+
+    print(lista.mostrar())
+
