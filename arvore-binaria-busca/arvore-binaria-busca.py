aa6cefc118e9d73d0260352258429a501585b6fa
deiz0n <eduardomacedosilva@outlook.com>
2024-12-12 10:09:01 -0300

    feat: implementação do método de inserção na árvore binária de busca

--- arvore-binaria-busca/arvore-binaria-busca.py
+++ arvore-binaria-busca/arvore-binaria-busca.py
@@ -4,6 +4,57 @@ class No:
         self.esquerda = None
         self.direita = None
 
+    def mostra_np(self):
+        print(self.valor)
+
 class ArvoreBinariaBusca:
     def __init__(self):
         self.raiz = None
+
+    def inserir(self, valor):
+        novo = No(valor)
+        if self.raiz is None:
+            self.raiz = novo
+        else:
+            atual = self.raiz
+
+            while True:
+                pai = atual
+                # Adiciona à esquerda
+                if valor < atual.valor:
+                    atual = atual.esquerda
+                    if atual is None:
+                        pai.esquerda = novo
+                        return
+                # Adiciona à direita
+                else:
+                    atual = atual.direita
+                    if atual is None:
+                        pai.direita = novo
+                        return
+
+
+    def __is_empty(self, novo):
+        if self.raiz is None:
+            self.raiz = novo
+
+if __name__ == "__main__":
+    arvore = ArvoreBinariaBusca()
+
+    arvore.inserir(53)
+    arvore.inserir(30)
+    arvore.inserir(14)
+    arvore.inserir(39)
+    arvore.inserir(9)
+    arvore.inserir(23)
+    arvore.inserir(34)
+    arvore.inserir(49)
+    arvore.inserir(72)
+    arvore.inserir(61)
+    arvore.inserir(84)
+    arvore.inserir(79)
+
+    print(arvore.raiz.esquerda.valor)
+    print(arvore.raiz.direita.valor)
+
+
