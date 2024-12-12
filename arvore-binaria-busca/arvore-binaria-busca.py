fc0ac36092d7c8f41cdee5da4154aebf7cf4f7db
deiz0n <eduardomacedosilva@outlook.com>
2024-12-12 09:29:37 -0300

    feat: implementação inicial da árvore binária de busca

---
+++ arvore-binaria-busca/arvore-binaria-busca.py
@@ -0,0 +1,9 @@
+class No:
+    def __init__(self, valor):
+        self.valor = valor
+        self.esquerda = None
+        self.direita = None
+
+class ArvoreBinariaBusca:
+    def __init__(self):
+        self.raiz = None
