ab933a3236a40b76070da06a3c84f19d93104659
deiz0n <eduardomacedosilva@outlook.com>
2024-12-25 18:18:45 -0300

    feat: adição da busca em largura

--- grafos/grafo.py
+++ grafos/grafo.py
@@ -1,6 +1,7 @@
 from grafos.vertice import Vertice
 from grafos.adjacente import Adjacente
 from pilha.pilha import Pilha
+from fila.fila_circular import FilaCircular
 
 
 class Grafo:
@@ -89,6 +90,34 @@ class Grafo:
                     self.buscar()
             print('Desempilhou: {}'.format(self.pilha.desempilhar().rotulo))
             print()
+
+    class BuscaLargura:
+        def __init__(self, inicio):
+            self.inicio = inicio
+            self.inicio = inicio.visitado = True
+            self.fila = FilaCircular(20)
+            self.fila.enfileirar(inicio)
+
+        def buscar(self):
+            primeiro = self.fila.primeiro()
+            print('-----')
+            print('Primeiro da fila: {}'.format(primeiro.rotulo))
+            temp = self.fila.desenfileirar()
+            print('Desenfilerou: {}'.format(temp.rotulo))
+            for adjacente in primeiro.adjacentes:
+                print(
+                    'Primeiro era: {}. {} já foi visitado? {}'
+                    .format(temp.rotulo,
+                            adjacente.vertice.rotulo,
+                            adjacente.vertice.visitado)
+                )
+                if adjacente.vertice.visitado is False:
+                    adjacente.vertice.visitado = True
+                    self.fila.enfileirar(adjacente.vertice)
+                    print('Enfileirou: {}'.format(adjacente.vertice.rotulo))
+            if self.fila.numeros_elementos > 0:
+                self.buscar()
+
 
 
 
--- grafos/main.py
+++ grafos/main.py
@@ -3,5 +3,7 @@ from grafo import Grafo
 if __name__ == '__main__':
     grafo = Grafo()
 
-    busca_profundidade = Grafo.BuscaProfundidade(grafo.arad)
-    busca_profundidade.buscar()
+    # busca_profundidade = Grafo.BuscaProfundidade(grafo.arad)
+    # busca_profundidade.buscar()
+    busca_largura = Grafo.BuscaLargura(grafo.dobreta)
+    busca_largura.buscar()
