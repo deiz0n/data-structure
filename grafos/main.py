64db3dd6c95efacda61a41dcdefc45f8951b8823
deiz0n <eduardomacedosilva@outlook.com>
2024-12-25 12:14:45 -0300

    test: Testes com o grafo criado

---
+++ grafos/main.py
@@ -0,0 +1,9 @@
+from grafo import Grafo
+
+if __name__ == '__main__':
+    grafo = Grafo()
+
+    grafo.arad.mostra_adjacentes()
+    print('--------------')
+    grafo.bucharest.mostra_adjacentes()
+
