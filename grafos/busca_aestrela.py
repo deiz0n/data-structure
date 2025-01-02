f598c2e26b52fa37980d9b11d27cdbc019db9082
deiz0n <eduardomacedosilva@outlook.com>
2025-01-02 15:55:24 -0300

    feat: implementação da busca a*

---
+++ grafos/busca_aestrela.py
@@ -0,0 +1,137 @@
+import numpy as np
+
+from grafos.busca_gulosa import Vertice
+
+
+class Adjacente:
+    def __init__(self, vertice, custo):
+        self.vertice = vertice
+        self.custo = custo
+        self.distancia_aestrela = vertice.distancia_objetivo + self.custo
+
+class Grafo:
+    arad = Vertice('Arad', 366)
+    zerind = Vertice('Zerind', 374)
+    oradea = Vertice('Oradea', 380)
+    sibiu = Vertice('Sibiu', 253)
+    timisoara = Vertice('Temisoara', 329)
+    lugoj = Vertice('Lugoj', 244)
+    mehadia = Vertice('Mehadia', 241)
+    dobreta = Vertice('Dobreta', 242)
+    craiova = Vertice('Craiova', 160)
+    rimnicu = Vertice('Rimncu', 193)
+    fagares = Vertice('Fagares', 178)
+    pitesti = Vertice('Pitesti', 98)
+    bucharest = Vertice('Bucharest', 0)
+    giurgiu = Vertice('Giurgiu',  77)
+
+    arad.adiciona_adjacente(Adjacente(zerind, 75))
+    arad.adiciona_adjacente(Adjacente(sibiu, 140))
+    arad.adiciona_adjacente(Adjacente(timisoara, 118))
+
+    zerind.adiciona_adjacente(Adjacente(arad, 75))
+    zerind.adiciona_adjacente(Adjacente(oradea, 71))
+
+    oradea.adiciona_adjacente(Adjacente(zerind, 71))
+    oradea.adiciona_adjacente(Adjacente(sibiu, 151))
+
+    sibiu.adiciona_adjacente(Adjacente(oradea, 151))
+    sibiu.adiciona_adjacente(Adjacente(arad, 140))
+    sibiu.adiciona_adjacente(Adjacente(fagares, 99))
+    sibiu.adiciona_adjacente(Adjacente(rimnicu, 80))
+
+    timisoara.adiciona_adjacente(Adjacente(arad, 118))
+    timisoara.adiciona_adjacente(Adjacente(arad, 111))
+
+    lugoj.adiciona_adjacente(Adjacente(timisoara, 111))
+    lugoj.adiciona_adjacente(Adjacente(mehadia, 70))
+
+    mehadia.adiciona_adjacente(Adjacente(lugoj, 70))
+    mehadia.adiciona_adjacente(Adjacente(dobreta, 75))
+
+    dobreta.adiciona_adjacente(Adjacente(mehadia, 75))
+    dobreta.adiciona_adjacente(Adjacente(craiova, 120))
+
+    craiova.adiciona_adjacente(Adjacente(dobreta, 120))
+    craiova.adiciona_adjacente(Adjacente(pitesti, 138))
+    craiova.adiciona_adjacente(Adjacente(rimnicu, 146))
+
+    rimnicu.adiciona_adjacente(Adjacente(craiova, 146))
+    rimnicu.adiciona_adjacente(Adjacente(sibiu, 80))
+    rimnicu.adiciona_adjacente(Adjacente(pitesti, 97))
+
+    fagares.adiciona_adjacente(Adjacente(sibiu, 99))
+    fagares.adiciona_adjacente(Adjacente(bucharest, 211))
+
+    pitesti.adiciona_adjacente(Adjacente(rimnicu, 97))
+    pitesti.adiciona_adjacente(Adjacente(craiova, 138))
+    pitesti.adiciona_adjacente(Adjacente(bucharest, 101))
+
+    bucharest.adiciona_adjacente(Adjacente(fagares, 211))
+    bucharest.adiciona_adjacente(Adjacente(pitesti, 101))
+    bucharest.adiciona_adjacente(Adjacente(giurgiu, 90))
+
+class VetorOrdenado:
+    def __init__(self, capacidade):
+        self.capacidade = capacidade
+        self.ultima_posicao = -1
+        self.valores = np.empty(capacidade, dtype=object)
+
+    def insere(self, adjacente):
+        if self.ultima_posicao == self.capacidade -1:
+            print('Capacidade máxima atinginda')
+            return
+        posicao = 0
+        for i in range(self.ultima_posicao + 1):
+            posicao = i
+            if self.valores[i].distancia_aestrela > adjacente.distancia_aestrela:
+                break
+            if i == self.ultima_posicao:
+                return  i + 1
+        x = self.ultima_posicao
+        while x >= posicao:
+            self.valores[x + 1] = self.valores[x]
+            x -= 1
+        self.valores[posicao] = adjacente
+        self.ultima_posicao += 1
+
+    def imprime(self):
+        if self.ultima_posicao == -1:
+            print('O vetor está cheio')
+        else:
+            for i in range(self.ultima_posicao + 1):
+                print(i , ' - ',
+                      self.valores[i].vertice.rotulo, ' - ',
+                      self.valores[i].custo, ' - ',
+                      self.valores[i].vertice.distancia_objetivo, ' - ',
+                      self.valores[i].distancia_aestrela
+                )
+
+class AEstrela():
+    def __init__(self, objetivo):
+        self.objetivo = objetivo
+        self.encontrado = False
+
+    def buscar(self, atual):
+        print('------------')
+        print('Atual: {}'.format(atual.rotulo))
+        atual.visitado = True
+
+        if atual == self.objetivo:
+            self.encontrado = True
+        else:
+            vetor = VetorOrdenado(len(atual.adjacentes))
+            for adjacente in atual.adjacentes:
+                if adjacente.vertice.visitado is False:
+                    adjacente.vertice.visitado = True
+                    vetor.insere(adjacente)
+            vetor.imprime()
+
+            if vetor.valores[0] is not None:
+                self.buscar(vetor.valores[0].vertice)
+
+if __name__ == '__main__':
+    grafo = Grafo()
+    busca_aestrela = AEstrela(grafo.bucharest)
+    busca_aestrela.buscar(grafo.arad)
+
